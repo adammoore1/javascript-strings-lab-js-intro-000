0338a341ab9a3c64d0402d010a5d2a92440d61bb
Adam Moore <adamjamesmoore@live.co.uk>
2020-11-21 21:29:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = 'Let\'s talks about space' +`${topic}`;
+var conversation = 'Let\'s talks about' +`${topic}`;
